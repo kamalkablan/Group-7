25c41dd90bff8f4d597612d0db9ac1d77632a310
hoshangwardak <hoshangwardak@gmail.com>
2020-11-12 13:46:08 -0500

    Finding the unique character

--- src/Hoshang/StringPractice1.java
+++ src/Hoshang/StringPractice1.java
@@ -3,5 +3,28 @@ package Hoshang;
 public class StringPractice1 {
     public static void main(String[] args) {
 
+        String str = "abfcabd";
+
+        String result = "";
+
+        for (int j=0; j<str.length(); j++) {
+            char firstIndex = str.charAt(j);
+            int counter = 0;
+            for (int i=0; i<str.length(); i++) {
+                if (str.charAt(i) == firstIndex) {
+                    counter++;
+                }
+            }
+            if (counter == 1) {
+                result += firstIndex;
+            }
+        }
+
+        System.out.println(result);
+
+
+
+
+
     }
 }
