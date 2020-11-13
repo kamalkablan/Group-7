ffa87516807d00130dbaa315558a49d8361144bf
minicandoit <74155239+minicandoit@users.noreply.github.com>
2020-11-13 07:10:20 -0500

    added more line

--- src/Mini/Task002.java
+++ src/Mini/Task002.java
@@ -5,7 +5,8 @@ import com.sun.xml.internal.ws.api.model.wsdl.WSDLOutput;
 public class Task002 {
 // more work here
 public static void main(String[] args) {
-
+//more change
+    //still in local branch
 }
 
 }
